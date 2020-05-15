f66481b9194b7a2ec9023b6ad000378e9d1a9232
Jeff Balagosa <jeff.balagosa@gmail.com>
2020-05-14 20:08:33 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $('#pic-search').on('keyup', function () {
-  const value = $(this).val().toLowerCase();
+  var value = $(this).val().toLowerCase();
   $('#thumbnails *').filter(function () {
-    $(this).toggle($(this.text().toLowerCase().indexOf(value) > -1);
+    $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
   });
 });
