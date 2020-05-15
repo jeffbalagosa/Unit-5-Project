f67f4d02bf7c9b75c92fcab48f8d97fffb8d6db5
Jeff Balagosa <jeff.balagosa@gmail.com>
2020-05-14 21:49:25 -0700

    more media query stuff and comments

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// For Exceeds.  "Project uses original JavaScript or jQuery code to implement search box functionality instead of a plugin."  Lots of Frankenstein codes from internet searches.
 $('#pic-search').keyup(function () {
   const val = $.trim(this.value);
   console.log(val);
